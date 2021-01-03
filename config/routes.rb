849738bde452cb63c76c357374166b4a0317f3ac
Horino Yutaro <suzy.city16@gmail.com>
2021-01-03 10:30:13 +0900

    [fix] routingの修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   devise_scope :user do
-    post 'users/guest_sign_in', to: 'users/sessions#new_guest'
+    post 'users/guest_sign_in', to: 'users/sessions#guest_login'
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
