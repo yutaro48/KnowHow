504375f184fa5f8c77106511234ac4368e352f85
Horino Yutaro <suzy.city16@gmail.com>
2021-01-03 17:45:21 +0900

    [add] showアクションの実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,16 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show]
 
   def index
     @posts = Post.all.order(created_at: :desc)
+  end
+
+  def show
+  end
+
+  private
+  def set_post
+    @post = Post.find(params[:id])
   end
 
 end
