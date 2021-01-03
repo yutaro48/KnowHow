bdcd7901dc35513e5adf78a9dc0b53947f6cd5b2
Horino Yutaro <suzy.city16@gmail.com>
2021-01-03 14:33:25 +0900

    [add] migrationの作成

--- db/migrate/20210103053039_create_posts.rb
+++ db/migrate/20210103053039_create_posts.rb
@@ -1,7 +1,8 @@
 class CreatePosts < ActiveRecord::Migration[6.0]
   def change
     create_table :posts do |t|
-
+      t.string :title, null: false
+      t.text :content, null: false
       t.timestamps
     end
   end
