f666c953aed859596773983b396ce6877c27256e
Horino Yutaro <suzy.city16@gmail.com>
2021-01-03 10:40:13 +0900

    [fix] check_guestメソッドをupdateアクションの前にも適用
    ゲストユーザのメールやパスワードを編集できないようにするため

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,9 +1,9 @@
 class Users::RegistrationsController < Devise::RegistrationsController
-  before_action :check_guest, only: :destroy
+  before_action :check_guest, only: [:update, :destroy]
   
   def check_guest
     if resource.email == 'guest@example.com'
-      redirect_to root_path, alert: 'ゲストユーザーは削除できません。'
+      redirect_to root_path, alert: 'ゲストユーザの編集・削除はできません。'
     end
   end
   
