bfbe5e4eb2fc1bb4e8b2aa103b261ed858c5b4f2
Horino Yutaro <suzy.city16@gmail.com>
2021-01-04 23:12:20 +0900

    [add] Action Textのインストール

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,3 +15,6 @@ require("bootstrap/dist/js/bootstrap")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+require("trix")
+require("@rails/actiontext")
