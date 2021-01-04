3ada6ef2da352249c49d69a10369d349220f88e8
Horino Yutaro <suzy.city16@gmail.com>
2021-01-04 19:57:30 +0900

    [add] updateアクションの実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  before_action :set_post, only: [:show, :edit]
+  before_action :set_post, only: [:show, :edit, :update]
 
   def index
     @posts = Post.all.order(created_at: :desc)
@@ -15,14 +15,23 @@ class PostsController < ApplicationController
   def create
     @post = Post.new(post_params)
     if @post.save
-        redirect_to post_path(@post), notice: 'アウトプットしました'
+        redirect_to post_path(@post), notice: 'アウトプットしました。'
     else
-        flash.now[:error] = 'アウトプットできませんでした'
+        flash.now[:error] = 'アウトプットできませんでした。'
         render :new
     end
   end
 
   def edit
+  end
+
+  def update
+    if @post.update(post_params)
+        redirect_to post_path(@post), notice: '更新しました。'
+    else
+        flash.now[:error] = '更新できませんでした。'
+        render :edit
+    end
   end
 
   private
