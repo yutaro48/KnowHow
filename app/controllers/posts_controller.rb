a137aec0e3a7ab6f5b4ce9bb2ae4b506c7bd3587
Horino Yutaro <suzy.city16@gmail.com>
2021-01-04 18:24:48 +0900

    [add] createアクションの実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,9 +12,23 @@ class PostsController < ApplicationController
     @post = Post.new
   end
 
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+        redirect_to post_path(@post), notice: 'アウトプットしました'
+    else
+        flash.now[:error] = 'アウトプットできませんでした'
+        render :new
+    end
+  end
+
   private
   def set_post
     @post = Post.find(params[:id])
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :content)
   end
 
 end
