6837b5372cff1282f9b78d4cb5378346c7cef703
Horino Yutaro <suzy.city16@gmail.com>
2021-01-04 23:19:21 +0900

    [remove] Postモデルからcontentカラムを削除

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_04_141003) do
+ActiveRecord::Schema.define(version: 2021_01_04_141643) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,7 +48,6 @@ ActiveRecord::Schema.define(version: 2021_01_04_141003) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title", null: false
-    t.text "content", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
--- test/fixtures/posts.yml
+++ test/fixtures/posts.yml
@@ -3,7 +3,6 @@
 # Table name: posts
 #
 #  id         :bigint           not null, primary key
-#  content    :text             not null
 #  title      :string           not null
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
