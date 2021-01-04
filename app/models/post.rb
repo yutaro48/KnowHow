6284eff88c3938053f4902da5ba188aaee628f55
Horino Yutaro <suzy.city16@gmail.com>
2021-01-04 23:15:32 +0900

    [add] Postモデルのcontentカラムにrich_textを適用

--- app/models/post.rb
+++ app/models/post.rb
@@ -9,6 +9,8 @@
 #  updated_at :datetime         not null
 #
 class Post < ApplicationRecord
+  has_rich_text :content
+
   validates :title, presence: true
   validates :title, length: { minimum: 2, maximum: 30 }
   validates :content, presence: true
