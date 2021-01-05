9ed5441b4f77f6dc185f3d38c1eac398691294e7
Horino Yutaro <suzy.city16@gmail.com>
2021-01-05 14:50:12 +0900

    [update] ログイン必須のアクションを定義

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
 
   def index
     @posts = Post.all.order(created_at: :desc)
