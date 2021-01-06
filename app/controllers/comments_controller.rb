61310d60237be6311879c67e9860119783426962
Horino Yutaro <suzy.city16@gmail.com>
2021-01-06 12:58:48 +0900

    [add] commentsのcreateアクションの実装

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,4 +3,20 @@ class CommentsController < ApplicationController
     post = Post.find(params[:post_id])
     @comment = post.comments.build
   end
+
+  def create
+    post = Post.find(params[:post_id])
+    @comment = post.comments.build(comment_params)
+    if @comment.save
+      redirect_to post_path(post), notice: 'コメントを追加しました。'
+    else
+      flash.now[:error] = '更新できませんでした。'
+      render :new
+    end
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:content)
+  end
 end
