869fa579a41aefeb7a406ccdd5293baf5cc4b7ee
Horino Yutaro <suzy.city16@gmail.com>
2021-01-07 22:15:57 +0900

    [add] profilesコントローラでupdateアクションを定義

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -8,4 +8,20 @@ class ProfilesController < ApplicationController
   def edit
     @profile = current_user.build_profile
   end
+
+  def update
+    @profile = current_user.build_profile(profile_params)
+    if @profile.save
+      redirect_to profile_path, notice: '更新しました。'
+    else
+      flash.now[:error] = '更新できませんでした。'
+      render :edit
+    end
+  end
+
+  private
+  def profile_params
+    params.require(:profile).permit(:name, :gender, :join, :birthday, :phone, :email)
+  end
+
 end
