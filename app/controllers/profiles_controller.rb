9a11b509950de0be8ea6780ed94c97a20e996430
Horino Yutaro <suzy.city16@gmail.com>
2021-01-08 11:25:48 +0900

    [add] profilesコントローラのStrongParameterにavatarを追加

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -22,7 +22,7 @@ class ProfilesController < ApplicationController
 
   private
   def profile_params
-    params.require(:profile).permit(:name, :gender, :join, :birthday, :phone, :email)
+    params.require(:profile).permit(:name, :gender, :join, :birthday, :phone, :email, :avatar)
   end
 
 end
