79a9045f4f738e968ddfa3cd2b355e7957797db8
Horino Yutaro <suzy.city16@gmail.com>
2021-01-08 18:10:58 +0900

    [fix] コミットあまり
    一覧表示や詳細表示もログインしていなければ見れないように修正

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,7 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show]
-  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+  before_action :authenticate_user!
+  # , only: [:new, :create, :edit, :update, :destroy]
 
   def index
     @posts = Post.all.order(created_at: :desc)
