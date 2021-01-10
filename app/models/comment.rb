288fcd45b70d8ccef40e539c2a300e9078b9dd44
Horino Yutaro <suzy.city16@gmail.com>
2021-01-10 16:20:13 +0900

    [add] Commentモデルにuser_idカラムを追加
    コメントを書いた人を動的に表示するため

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -7,10 +7,12 @@
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
 #  post_id    :bigint           not null
+#  user_id    :bigint
 #
 # Indexes
 #
 #  index_comments_on_post_id  (post_id)
+#  index_comments_on_user_id  (user_id)
 #
 class Comment < ApplicationRecord
   belongs_to :post
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_08_091214) do
+ActiveRecord::Schema.define(version: 2021_01_10_071216) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -60,7 +60,9 @@ ActiveRecord::Schema.define(version: 2021_01_08_091214) do
     t.text "content", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id"
     t.index ["post_id"], name: "index_comments_on_post_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "posts", force: :cascade do |t|
