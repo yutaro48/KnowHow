7a379f8eefaf6d3332c5a4b806bd1c4684fb8fbd
Horino Yutaro <suzy.city16@gmail.com>
2021-01-10 16:47:07 +0900

    [update] commentのストロングパラメータにuser_idカラムを追加

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -17,6 +17,6 @@ class CommentsController < ApplicationController
 
   private
   def comment_params
-    params.require(:comment).permit(:content)
+    params.require(:comment).permit(:content).merge(user_id: current_user.id)
   end
 end
