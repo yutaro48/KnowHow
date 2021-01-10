3c1a71e9e5a71db3951bc36015b45e87dbf51db7
Horino Yutaro <suzy.city16@gmail.com>
2021-01-10 20:06:45 +0900

    [add] UserモデルとRelationshipモデルを紐付け

--- app/models/user.rb
+++ app/models/user.rb
@@ -34,6 +34,12 @@ class User < ApplicationRecord
   has_many :bookmarks, dependent: :destroy
   has_many :bookmarked_posts, through: :bookmarks, source: :post
 
+  has_many :following_relationships, foreign_key: 'follower_id', class_name: 'Relationship', dependent: :destroy
+  has_many :followings, through: :following_relationships, source: :following
+
+  has_many :follower_relationships, foreign_key: 'following_id', class_name: 'Relationship', dependent: :destroy
+  has_many :followers, through: :follower_relationships, source: :follower
+
   delegate :birthday, :age, :join_age, :gender, :phone, :mail, :join, to: :profile, allow_nil: true
 
   def has_outputted?(post)
