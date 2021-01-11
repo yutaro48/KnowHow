1d5103d7d9a78456ce839f969a8e389212bc06cb
Horino Yutaro <suzy.city16@gmail.com>
2021-01-11 14:36:39 +0900

    [update] followsコントローラのindexアクションを定義

--- app/controllers/follows_controller.rb
+++ app/controllers/follows_controller.rb
@@ -2,6 +2,11 @@ class FollowsController < ApplicationController
   before_action :authenticate_user!
 
   def index
+    @user = User.find(params[:account_id])
+
+    if @user == current_user
+      redirect_to profile_path
+    end
   end
   
   def create
