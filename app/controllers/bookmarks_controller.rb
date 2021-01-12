cf70ad675b756c42ddba808c8a56f953e5ac14c3
Horino Yutaro <suzy.city16@gmail.com>
2021-01-12 16:11:22 +0900

    [add] bookmarksコントローラのshowアクションを定義

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,6 +1,12 @@
 class BookmarksController < ApplicationController
   before_action :authenticate_user!
 
+  def show
+    post = Post.find(params[:post_id])
+    bookmark_status = current_user.has_bookmarked?(post)
+    render json: { hasBookmarmed: bookmark_status }
+  end
+
   def create
     post = Post.find(params[:post_id])
     post.bookmarks.create!(user_id: current_user.id)
