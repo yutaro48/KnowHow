9e2dfb51498569e2010e8b253c94f4a97401ff56
Horino Yutaro <suzy.city16@gmail.com>
2021-01-12 16:03:25 +0900

    [update] bookmarkのroutingを更新

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,7 @@ Rails.application.routes.draw do
   resources :posts do
     resources :comments, only: [:new, :create]
 
-    resource :bookmark, only: [:create, :destroy]
+    resource :bookmark, only: [:show, :create, :destroy]
   end
 
   resource :profile, only: [:show, :edit, :update]
