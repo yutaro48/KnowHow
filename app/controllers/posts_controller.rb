599d280fbefe6a9211944da9f9715dea6775c53a
Horino Yutaro <suzy.city16@gmail.com>
2021-01-13 20:11:03 +0900

    [update] postsコントローラのindexアクションに検索用のインスタンス変数を定義

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,6 +5,7 @@ class PostsController < ApplicationController
 
   def index
     @posts = Post.all.order(created_at: :desc)
+    @posts = Post.search(params["q"]).order(created_at: :desc)
   end
 
   def show
