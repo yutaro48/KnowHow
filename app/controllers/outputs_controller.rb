1386d927f6d4dafa92967e72dc1bb9aae4e93a11
Horino Yutaro <suzy.city16@gmail.com>
2021-01-14 14:03:56 +0900

    [fix] outputsページで公開記事のみ表示するよう修正

--- app/controllers/outputs_controller.rb
+++ app/controllers/outputs_controller.rb
@@ -2,7 +2,7 @@ class OutputsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @posts = current_user.posts.order(created_at: :desc)
+    @posts = current_user.posts.published.order(created_at: :desc)
     @posts = @posts.page(params[:page]).per(5).order(created_at: :desc)
   end
 end
