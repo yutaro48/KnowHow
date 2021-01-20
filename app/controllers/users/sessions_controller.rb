2ea691f9dc7d1c036d82a37de3bb8c3026abffb2
Horino Yutaro <suzy.city16@gmail.com>
2021-01-20 22:22:46 +0900

    [fix] ログイン後posts#indexにリダイレクトするように修正

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -2,6 +2,6 @@ class Users::SessionsController < Devise::SessionsController
   def guest_login
     user = User.find_guest
     sign_in user
-    redirect_to root_path, notice: 'ゲストユーザーとしてログインしました。'
+    redirect_to posts_path, notice: 'ゲストユーザーとしてログインしました。'
   end
 end
