7de0e990268494ba7cac6373e65b1e365b80f7a2
Horino Yutaro <suzy.city16@gmail.com>
2021-01-20 18:37:10 +0900

    [fix] commentsコントローラの修正

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,6 +2,8 @@ class CommentsController < ApplicationController
   def new
     post = Post.find(params[:post_id])
     @comment = post.comments.build
+
+    @histories = current_user.histories
   end
 
   def create
