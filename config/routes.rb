d9a23a4cecd63b9ee0e1bcd28581538d257f05fc
Horino Yutaro <suzy.city16@gmail.com>
2021-01-20 19:20:18 +0900

    [fix] エラー対応のためroot_pathをhome#indexに変更

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  root to: "posts#index"
+  root to: "home#index"
 
   resources :posts do
     resources :comments, only: [:new, :create]
