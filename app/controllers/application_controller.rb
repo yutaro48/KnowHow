6e061d4983231f1d683662040ad8f3c914acd2e9
Horino Yutaro <suzy.city16@gmail.com>
2021-01-22 01:59:47 +0900

    [fix] ログイン時のリダイレクト先をposts#indexに変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,12 @@ class ApplicationController < ActionController::Base
     ActiveDecorator::Decorator.instance.decorate(super) if super.present?
     super
   end
+
+  def after_sign_up_path_for(resource)
+    posts_path(resource)
+  end
+
+  def after_sign_in_path_for(resource)
+    posts_path(resource)
+  end
 end
