d94aba2f7f87468af093ff8dea752950922ac999
Horino Yutaro <suzy.city16@gmail.com>
2021-01-22 02:06:05 +0900

    [add] ログイン後のリダイレクト先をroot_pathに変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,4 +11,8 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
     posts_path(resource)
   end
+
+  def after_sign_out_path_for(resource)
+    root_path(resource)
+  end
 end
