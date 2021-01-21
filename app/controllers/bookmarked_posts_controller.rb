ee0f3186043e07931f487dacccec89a100282875
Horino Yutaro <suzy.city16@gmail.com>
2021-01-22 00:31:36 +0900

    [fix] ページネーションの上限を６に設定

--- app/controllers/bookmarked_posts_controller.rb
+++ app/controllers/bookmarked_posts_controller.rb
@@ -3,6 +3,6 @@ class BookmarkedPostsController < ApplicationController
 
   def index
     @posts = current_user.bookmarked_posts.published.order(created_at: :desc)
-    @posts = @posts.page(params[:page]).per(5).order(created_at: :desc)
+    @posts = @posts.page(params[:page]).per(6).order(created_at: :desc)
   end
 end
