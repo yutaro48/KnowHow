1115ecc576fbe571939f2e53ce23dd6cdd5037a9
Horino Yutaro <suzy.city16@gmail.com>
2021-01-21 21:28:37 +0900

    [remove] profilesコントローラのindexアクションを削除

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,10 +1,6 @@
 class ProfilesController < ApplicationController
   before_action :authenticate_user!
 
-  def show
-    @profile = current_user.profile
-  end
-
   def edit
     @profile = current_user.prepare_profile
   end
