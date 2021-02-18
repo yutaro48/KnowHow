758075d00ce7152ee012fde3e04da51f556f0236
Horino Yutaro <suzy.city16@gmail.com>
2021-02-18 20:45:16 +0900

    [add] Postのタイトルと内容が入力されていたらノウハウを作成できる

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,4 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
+  it 'タイトルと内容が入力されていれば、ノウハウを保存できる' do
+    user = User.create!({
+      email: 'test@example.com',
+      password: 'password'
+    })
+    post = user.posts.build({
+      title: Faker::Lorem.characters(number: 10),
+      content: Faker::Lorem.characters(number: 100)
+    })
+
+    expect(post).to be_valid
+  end
 end
