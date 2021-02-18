34ef0270d5f6f49eb9a5df401fee32c7539e1509
Horino Yutaro <suzy.city16@gmail.com>
2021-02-18 22:46:04 +0900

    [add] postsコントローラのGETリクエストに関するテスト

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,10 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe "Posts", type: :request do
+  let!(:user) { create(:user) }
+  let!(:posts) { create_list(:post, 3, user: user) }
+
   describe "GET /posts" do
-    it "200ステータスが返ってくる" do
-      get posts_path
-      expect(response).to have_http_status(200)
+    context 'ログインしている場合' do
+      before do
+        sign_in user
+      end
+
+      it "200ステータスが返ってくる" do
+        get posts_path
+        expect(response).to have_http_status(200)
+      end
     end
   end
 end
