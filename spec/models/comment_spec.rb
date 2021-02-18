fa405f6a05cebcc1fe374937343d2f3e8dc0075b
Horino Yutaro <suzy.city16@gmail.com>
2021-02-18 21:36:08 +0900

    [add] Commentの内容が入力されている場合のテスト

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let!(:user) { create(:user) }
+
+  context '内容が入力されている場合' do
+    let!(:post) { build(:post, user: user) }
+    let!(:comment) { build(:comment, user: user, post: post) }
+
+    it 'コメントを保存できる' do
+      expect(comment).to be_valid
+    end
+  end
 end
