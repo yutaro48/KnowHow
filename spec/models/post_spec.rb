68878160b9f537f443449dc92e8aa1240153c17b
Horino Yutaro <suzy.city16@gmail.com>
2021-02-18 20:51:49 +0900

    [update] contextを使用して書き換え

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,16 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  it 'タイトルと内容が入力されていれば、ノウハウを保存できる' do
-    user = User.create!({
-      email: 'test@example.com',
-      password: 'password'
-    })
-    post = user.posts.build({
-      title: Faker::Lorem.characters(number: 10),
-      content: Faker::Lorem.characters(number: 100)
-    })
+  context 'タイトルと内容が入力されている場合' do
+    before do
+      user = User.create!({
+        email: 'test@example.com',
+        password: 'password'
+      })
+      @post = user.posts.build({
+        title: Faker::Lorem.characters(number: 10),
+        content: Faker::Lorem.characters(number: 100)
+      })
+    end
 
-    expect(post).to be_valid
+    it 'ノウハウを保存できる' do
+      expect(@post).to be_valid
+    end
   end
 end
