d6c4ab903e043d557cccae77f723e75066ed2eb2
Horino Yutaro <suzy.city16@gmail.com>
2021-02-19 02:52:02 +0900

    [add] postsの一覧表示画面のテスト

--- spec/system/post_spec.rb
+++ spec/system/post_spec.rb
@@ -1,7 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe 'Post', type: :system do
-  it 'ノウハウ一覧が表示される' do
-    visit root_path
+  let!(:user) { create(:user) }
+  let!(:posts) { create_list(:post, 3, status: 1, user: user) }
+
+  context 'ログインしている場合' do
+    before do
+      sign_in user
+    end
+
+    it 'ノウハウ一覧が表示される' do
+      visit posts_path
+      posts.each do |post|
+        expect(page).to have_content(post.title)
+      end
+    end
   end
 end
