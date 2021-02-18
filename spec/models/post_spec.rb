13deed51e062ccd1fcf5dda128a853b20de885d3
Horino Yutaro <suzy.city16@gmail.com>
2021-02-18 21:07:36 +0900

    [add] タイトルが１文字の場合のテスト

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,14 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  context 'タイトルと内容が入力されている場合' do
-    let!(:user) do
-      User.create!({
-        email: 'test@example.com',
-        password: 'password'
-      })
-    end
+  let!(:user) do
+    User.create!({
+      email: 'test@example.com',
+      password: 'password'
+    })
+  end
 
+  context 'タイトルと内容が入力されている場合' do
     let!(:post) do
       user.posts.build({
         title: Faker::Lorem.characters(number: 10),
@@ -17,7 +17,20 @@ RSpec.describe Post, type: :model do
     end
 
     it 'ノウハウを保存できる' do
-      expect(@post).to be_valid
+      expect(post).to be_valid
+    end
+  end
+
+  context 'タイトルの文字が1文字の場合' do
+    let!(:post) do
+      user.posts.create({
+        title: Faker::Lorem.characters(number: 1),
+        content: Faker::Lorem.characters(number: 100)
+      })
+    end
+
+    it 'ノウハウを保存できない' do
+      expect(post.errors.messages[:title][0]).to eq('は2文字以上で入力してください')
     end
   end
 end
