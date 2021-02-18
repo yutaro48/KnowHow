93b31d6ae8609e9c310b50f289c6c836e70b467d
Horino Yutaro <suzy.city16@gmail.com>
2021-02-18 23:05:39 +0900

    [add] postsコントローラのログインしていない場合のテスト

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -17,6 +17,15 @@ RSpec.describe "Posts", type: :request do
     end
   end
 
+  describe "GET /posts" do
+    context 'ログインしていない場合' do
+      it "ログイン画面に遷移する" do
+        get posts_path
+        expect(response).to redirect_to(new_user_session_path)
+      end
+    end
+  end
+
   describe 'POST /posts' do
     context 'ログインしている場合' do
       before do
@@ -29,6 +38,16 @@ RSpec.describe "Posts", type: :request do
         expect(response).to have_http_status(302)
         expect(Post.last.title).to eq(post_params[:title])
         expect(Post.last.content.body.to_plain_text).to eq(post_params[:content])
+      end
+    end
+  end
+
+  describe 'POST /posts' do
+    context 'ログインしていない場合' do
+      it 'ログイン画面に遷移する' do
+        post_params = attributes_for(:post)
+        post posts_path(post: post_params)
+        expect(response).to redirect_to(new_user_session_path)
       end
     end
   end
