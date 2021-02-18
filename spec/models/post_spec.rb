a68d006471a1bdf4834d29b7180b74d9414a5e75
Horino Yutaro <suzy.city16@gmail.com>
2021-02-18 21:18:01 +0900

    [update] users.rbの内容をpost_spec.rbに適用

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,12 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  let!(:user) do
-    User.create!({
-      email: 'test@example.com',
-      password: 'password'
-    })
-  end
+  let!(:user) { create(:user) }
 
   context 'タイトルと内容が入力されている場合' do
     let!(:post) do
