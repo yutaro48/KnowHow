e8017ef5a0f95e68290ef69f789a2a450b4c2f70
Horino Yutaro <suzy.city16@gmail.com>
2021-02-19 12:46:15 +0900

    [add] 環境変数の設定を追加

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,11 @@ module KnowHow
     # タイムゾーンを変換するために追加
     config.time_zone = 'Asia/Tokyo'
 
+    if Rails.env.development? || Rails.env.test?
+      Bundler.require(*Rails.groups)
+      Dotenv::Railtie.load
+    end
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
